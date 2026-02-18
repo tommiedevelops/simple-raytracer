d748c2788511b14b865c7cddca4103f697ef99c3
tommiedevelops <thomas.dizon25@gmail.com>
2026-02-18 11:28:37 +1100

    added test ppm printer

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cpp-raytracer
-A simple ray tracer developed in C++
+
+## A simple ray tracer developed in C++
+
+Based on the tutorial series by Peter Shirley, Trevor Black & Steve Hollasch.
+
+[Ray Tracing in One Weekend](https://raytracing.github.io/books/
+            RayTracingInOneWeekend.html)
+
+
+
--- SimpleRayTracer.cpp
+++ SimpleRayTracer.cpp
@@ -1,20 +1,27 @@
-// SimpleRayTracer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Outputting to ppm format
+
+    int image_width  = 10;
+    int image_height = 10;
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+
+            auto r = double(i) / image_width - 1;
+            auto g = double(j) / image_height - 1;
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
